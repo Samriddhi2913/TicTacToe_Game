31982252e680104661414796e616d1beae09e60d
Samriddhi2913 <samriddhisingh3004@gmail.com>
2025-10-02 22:53:36 +0530

    Update JS file

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ boxes.forEach((box)=>{
 
 const checkWinner=()=>{
     for(let pattern of winPatterns){
-        console.log(pattern);
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos2Val != "" && pos3Val !=" "){
+            if(pos1Val==pos2Val && pos2Val==pos3Val){
+                console.log("Winner");
+            }
+        }
     }  
 }
